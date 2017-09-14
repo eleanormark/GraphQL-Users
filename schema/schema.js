8eb8df216ece54b3277bd97e8ad31957fea2d61c
Eleanor Mark <eleanormark@gmail.com>
2017-09-14 13:05:34 -0700

    add UserType in GraphQL Schema

---
+++ schema/schema.js
@@ -0,0 +1,19 @@
+const graphql = require('graphql');
+const axios = require('axios');
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull
+} = graphql;
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields: {
+        id: { type: GraphQLString},
+        firstName: { type: GraphQLString},
+        age: { type: GraphQLInt }
+    }
+});
