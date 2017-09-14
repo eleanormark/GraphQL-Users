3736253eed2bd7bc5087bdaa0a08439eae63c0ad
Eleanor Mark <eleanormark@gmail.com>
2017-09-14 14:48:45 -0700

    add axios async resolve function

--- schema/schema.js
+++ schema/schema.js
@@ -1,5 +1,4 @@
 const graphql = require('graphql');
-const _ = require('lodash');
 const axios = require('axios');
 const {
   GraphQLObjectType,
@@ -9,11 +8,6 @@ const {
   GraphQLList,
   GraphQLNonNull
 } = graphql;
-
-const users = [
-    { id: '23', firstName: 'Bill', age: 20},
-    { id: '47', firstName: 'Samantha', age: 21}
-];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -28,10 +22,11 @@ const RootQuery = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
         user: {
-            type: UserType,
-            args: { id: { type: GraphQLString }},
-            resolve(parentValue, args) {
-                return _.find(users, {id: args.id});
+          type: UserType,
+          args: { id: { type: GraphQLString } },
+          resolve(parentValue, args) {
+                return axios.get(`http://localhost:3000/users/${args.id}`)
+                        .then(response => response.data);
             }
         }
     }
